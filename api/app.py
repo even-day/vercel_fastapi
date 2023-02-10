edeb4a293b184b0786385e8bf85c33664a9397cc
hasee <even_day@outlook.com>
2023-02-10 14:22:21 +0800

    添加clash

--- api/app.py
+++ api/app.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+
 from api.v2 import get_sub_url
+from api.clash import main
 
 app = FastAPI()
 
@@ -13,3 +16,8 @@ async def root():
 def v2():
     subscription_dict = get_sub_url()
     return subscription_dict
+
+@app.get('/clash',response_class=PlainTextResponse)
+def clash():
+    content = main()
+    return content
