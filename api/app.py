a2941c03fe4147ce24ac40f1d9cb208021f12747
HA2 <even_day@yeah.net>
2023-04-22 15:12:38 +0800

    添加raw

--- api/app.py
+++ api/app.py
@@ -4,6 +4,8 @@ from fastapi.responses import PlainTextResponse
 from api.v2 import get_sub_url
 from api.clash import main
 from api.clash2 import main2
+from api.raw import raw
+
 app = FastAPI()
 
 
@@ -17,12 +19,20 @@ def v2():
     subscription_dict = get_sub_url()
     return subscription_dict
 
-@app.get('/clash',response_class=PlainTextResponse)
+
+@app.get('/clash', response_class=PlainTextResponse)
 def clash():
     content = main()
     return content
 
-@app.get('/clash2',response_class=PlainTextResponse)
+
+@app.get('/clash2', response_class=PlainTextResponse)
 def clash2():
     content = main2()
+    return content
+
+
+@app.get('/raw/{link}', response_class=PlainTextResponse)
+def clash2(link):
+    content = raw(link)
     return content
---
+++ api/raw.py
@@ -0,0 +1,9 @@
+import requests
+
+
+def raw(url):
+    headers = {
+        'User-Agent': 'Rule2Clash'
+    }
+    response = requests.get(url, headers=headers, timeout=5000).text
+    return response
