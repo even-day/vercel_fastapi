0a777c83e8ff1c9f8d438ccfe5af6b2ec5d84e03
HA2 <even_day@yeah.net>
2023-04-24 22:42:44 +0800

    允许跨域

--- api/app.py
+++ api/app.py
@@ -6,8 +6,21 @@ from api.clash import main
 from api.clash2 import main2
 from api.raw import return_raw
 from api.one import get_hub_url
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# 声明一个 源 列表；重点：要包含跨域的客户端 源
+origins = ["https://node-drw.pages.dev/"]
+
+# 配置 CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # 允许访问的源
+    allow_credentials=True,  # 支持 cookie
+    allow_methods=["*"],  # 允许使用的请求方法
+    allow_headers=["*"]  # 允许携带的 Headers
+)
 
 
 @app.get("/")
